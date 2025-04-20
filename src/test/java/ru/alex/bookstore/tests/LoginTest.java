83063debec33a00b545ff80168fb3dbf66d10dc9
alex <apribylov90@mail.ru>
2025-04-20 15:25:47 +0300

    added test successful login modal

---
+++ src/test/java/ru/alex/bookstore/tests/LoginTest.java
@@ -0,0 +1,36 @@
+package ru.alex.bookstore.tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.alex.bookstore.pages.MainPage;
+import ru.alex.bookstore.pages.modal.LoginModal;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Epic("Личный Кабинет")
+@Feature("Авторизация")
+@DisplayName("Тесты авторизации")
+public class LoginTest extends BaseTest {
+
+    @Story("Проверка формы входа")
+    @Description("Проверяем, что пользователь может войти в систему с валидными учетными данными")
+    @DisplayName("Успешный вход с корректными данными")
+    @Test
+    public void loginModalSuccessTest() {
+        LoginModal loginModalPage = mainPage.openLoginModal();
+
+        assertTrue(loginModalPage.isAuthTitlePresent());
+
+        MainPage authMainPage = loginModalPage.setEmail(authConfig.login())
+                .setPassword(authConfig.password())
+                .clickRememberMeCheckbox()
+                .clickLoginButton();
+
+        assertTrue(authMainPage.isLoggedIn(authConfig.login()), "Должно быть видно имя пользователя");
+
+    }
+}
