b6270af020cdf4e8bffa650c368d673480ea5e9b
alex <apribylov90@mail.ru>
2025-04-20 16:08:38 +0300

    created SearchResultsPage for search tests

---
+++ src/test/java/ru/alex/bookstore/pages/SearchResultsPage.java
@@ -0,0 +1,39 @@
+package ru.alex.bookstore.pages;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class SearchResultsPage extends BasePage {
+    private final By searchResults = By.id("searchresults");
+    private final By searchTitle = By.className("page-header__title");
+    private final By loader = By.xpath("//p[@id='pLoader']");
+    private final By authorName = By.className("author-name");
+
+    public SearchResultsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Step("Проверка исчез ли loader")
+    public boolean isLoaderDisappeared() {
+        try {
+            return wait.until(ExpectedConditions.invisibilityOfElementLocated(loader));
+        } catch (TimeoutException e) {
+            System.out.println("Loader не исчез");
+            return false;
+        }
+    }
+
+    @Step("Проверка видны ли результаты поиска")
+    public boolean searchResultsArePresent() {
+        return isElementPresent(searchResults);
+    }
+
+    @Step("Проверка виден ли заголовок после удачного поиска")
+    public String searchTitleIsPresent() {
+        return find(searchTitle).getText();
+    }
+}
