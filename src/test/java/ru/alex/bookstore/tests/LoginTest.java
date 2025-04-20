7b085491ed13b5209e9b4d750ffd4b0f2b4ea5f2
alex <apribylov90@mail.ru>
2025-04-20 16:03:19 +0300

    added incorrect login test

--- src/test/java/ru/alex/bookstore/tests/LoginTest.java
+++ src/test/java/ru/alex/bookstore/tests/LoginTest.java
@@ -6,6 +6,7 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import ru.alex.bookstore.pages.LoginPage;
 import ru.alex.bookstore.pages.MainPage;
 import ru.alex.bookstore.pages.modal.LoginModal;
 
@@ -33,4 +34,26 @@ public class LoginTest extends BaseTest {
         assertTrue(authMainPage.isLoggedIn(authConfig.login()), "Должно быть видно имя пользователя");
 
     }
+
+    @Story("Проверка формы входа")
+    @Description("Проверяем, что пользователь не может войти в систему с невалидными учетными данными")
+    @DisplayName("Ошибка входа с некорректными данными")
+    @Test
+    public void loginErrorTest() {
+        LoginPage loginPage = mainPage.openLoginPage();
+
+        assertTrue(loginPage.isAuthTitlePresent());
+
+        String invalidEmail = "test@test.com";
+        loginPage
+                .setEmail(invalidEmail)
+                .setPassword(authConfig.password())
+                .clickRememberMeCheckbox()
+                .clickLoginButton();
+
+        assertTrue(loginPage.isAuthErrorPresent(), "Должно быть видно сообщение об ошибке в имени или пароле");
+
+    }
+
+
 }
