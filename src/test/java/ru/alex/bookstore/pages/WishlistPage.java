ed7b73d89853c265e44c947ad606ff3c82e8a612
alex <apribylov90@mail.ru>
2025-04-20 18:35:50 +0300

    created wishlist page

---
+++ src/test/java/ru/alex/bookstore/pages/WishlistPage.java
@@ -0,0 +1,19 @@
+package ru.alex.bookstore.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WishlistPage extends BasePage {
+    private final By wishlistItems = By.cssSelector(".catalog__list .row");
+    private final By wishlistTitle = By.className("page-header__title");
+
+    public WishlistPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public int booksCount() {
+        WebElement booksRow = find(wishlistItems);
+        return booksRow.findElements(By.cssSelector(".catalog__item")).size();
+    }
+}
