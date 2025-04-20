5446f1c4c072c21585e2b130731622cad86b9d0e
alex <apribylov90@mail.ru>
2025-04-20 17:25:53 +0300

    created header page

---
+++ src/test/java/ru/alex/bookstore/pages/elements/HeaderElement.java
@@ -0,0 +1,22 @@
+package ru.alex.bookstore.pages.elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.alex.bookstore.pages.BasePage;
+
+public class HeaderElement extends BasePage {
+    private final By wishlistButton = By.className("header__main__favorites__block");
+    private final By wishlistCount = By.id("wish_circle");
+    private final By basketButton = By.className("header__main__basket__block");
+    private final By basketCount = By.id("basket_circle");
+
+
+    public HeaderElement(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getWishlistCount() {
+        return find(wishlistCount).getText();
+    }
+
+}
