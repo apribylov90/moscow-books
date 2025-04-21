75a7b2dce3b759d1c0571f9a6945ebeed430f9da
alex <apribylov90@mail.ru>
2025-04-21 17:29:15 +0300

    added steps for allure; changed assert

--- src/test/java/ru/alex/bookstore/tests/WishlistTests.java
+++ src/test/java/ru/alex/bookstore/tests/WishlistTests.java
@@ -35,9 +35,7 @@ public class WishlistTests extends BaseTest {
         SearchResultsPage searchResultsPage = mainPage.searchFor("Пушкин");
 
         step("Ожидание исчезновения loader", () -> {
-            assertThat(searchResultsPage.isLoaderDisappeared())
-                    .as("Loader должен исчезнуть")
-                    .isTrue();
+            assertThat(searchResultsPage.isLoaderDisappeared()).isTrue();
         });
 
         step("Проверка наличия результатов поиска", () -> {
@@ -64,12 +62,14 @@ public class WishlistTests extends BaseTest {
 
         WishlistPage wishlistPage = header.clickWishlistButton();
 
-        step("Проверка Количества книг в избранном", () -> {
+        step("Проверка количества книг в избранном", () -> {
             assertThat(wishlistPage.booksCount()).isEqualTo(1);
         });
     }
 
-
+    @Story("Проверка функционала удаления из избранного")
+    @Description("Проверяем, что пользователь может успешно удалить товар из избранного")
+    @DisplayName("Успешное удаление из избранного")
     @Test
     public void removeFromWishlistTest() {
         String expectedMessage = "Товар удален из избранного";
@@ -79,25 +79,27 @@ public class WishlistTests extends BaseTest {
                 "ASP.NET_SessionId"
         );
 
-        try {
-            mainPage.openMainPage();
+        step("Добавить книгу в избранное через API", () -> {
+            try {
+                mainPage.openMainPage();
 
-            Map<String, String> filteredCookies = driver.manage().getCookies().stream()
-                    .filter(cookie -> neededCookies.contains(cookie.getName()))
-                    .collect(Collectors.toMap(Cookie::getName, Cookie::getValue));
+                Map<String, String> filteredCookies = driver.manage().getCookies().stream()
+                        .filter(cookie -> neededCookies.contains(cookie.getName()))
+                        .collect(Collectors.toMap(Cookie::getName, Cookie::getValue));
 
-            addToWishList(filteredCookies);
+                addToWishList(filteredCookies);
 
-        } catch (NullPointerException e) {
-            System.out.println("Нет нужных cookie");
-        }
+            } catch (NullPointerException e) {
+                System.out.println("Нет нужных cookie");
+            }
+        });
 
         WishlistPage wishlistPage = mainPage.openWishPage();
         assertThat(wishlistPage.booksCount()).isGreaterThan(0);
 
         wishlistPage.removeBook();
 
-        step("Проверка всплывающего сообщения - Товар помещен в избранное", () -> {
+        step("Проверка всплывающего сообщения - Товар удален из избранного", () -> {
             assertThat(popup.popupAppeared())
                     .as("Должно появиться сообщение об удалении в избранное")
                     .isTrue();
@@ -105,7 +107,10 @@ public class WishlistTests extends BaseTest {
             assertThat(popup.getPopupMessage()).isEqualTo(expectedMessage);
         });
 
-        assertThat(wishlistPage.booksCount()).isEqualTo(0);
+        step("Проверка количества книг в избранном", () -> {
+            assertThat(wishlistPage.booksCount()).isEqualTo(0);
+        });
+
 
     }
 }
