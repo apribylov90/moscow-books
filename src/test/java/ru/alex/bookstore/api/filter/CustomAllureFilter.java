72d89a5fe3f541a9e7a53beb8d484fe39e066eed
alex <apribylov90@mail.ru>
2025-04-21 12:54:29 +0300

    added allure report

---
+++ src/test/java/ru/alex/bookstore/api/filter/CustomAllureFilter.java
@@ -0,0 +1,13 @@
+package ru.alex.bookstore.api.filter;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+
+public class CustomAllureFilter {
+
+    public static AllureRestAssured withCustomAllureFilter() {
+        AllureRestAssured filter = new AllureRestAssured();
+        filter.setRequestTemplate("request-template.ftl");
+        filter.setResponseTemplate("response-template.ftl");
+        return filter;
+    }
+}
--- src/test/java/ru/alex/bookstore/api/tests/WishlistTests.java
+++ src/test/java/ru/alex/bookstore/api/tests/WishlistTests.java
@@ -1,46 +1,56 @@
 package ru.alex.bookstore.api.tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.alex.bookstore.api.models.wishlist.WishlistPushResponseModel;
 import ru.alex.bookstore.api.models.wishlist.WishlistResponseModel;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static ru.alex.bookstore.api.filter.CustomAllureFilter.withCustomAllureFilter;
 
-
+@DisplayName("Проверка Wishlist API")
 public class WishlistTests extends BaseTest {
 
+    @DisplayName("Успешное добавление книги в избранное")
     @Test
     public void addToWishListTest() {
         String body = "{\"event\":\"wish\",\"shop_id\":\"e33fc52bd51526cc6b0fe4e5e18401\",\"did\":\"AH5aEvmlqn\",\"sid\":\"cIxzHQJbUo\",\"seance\":\"cIxzHQJbUo\",\"segment\":\"B\",\"items\":[{\"id\":\"1133885A\"}],\"referer\":\"https://www.moscowbooks.ru/book/1133885/\"}";
 
-
         WishlistResponseModel wishlistResponse = given()
+                .filter(withCustomAllureFilter())
                 .contentType("application/x-www-form-urlencoded")
                 .formParam("producttype", "0")
                 .formParam("productid", "1133885")
                 .when()
                 .post("https://www.moscowbooks.ru/basket/addwish")
                 .then()
-//                .log().body()
+                .statusCode(200)
                 .extract().as(WishlistResponseModel.class);
 
-        assertThat(wishlistResponse.getCnt()).isEqualTo(1);
-        assertThat(wishlistResponse.getIsSuccess()).isTrue();
-
+        step("Проверка ответа", () -> {
+            assertThat(wishlistResponse.getCnt()).isEqualTo(1);
+            assertThat(wishlistResponse.getIsSuccess()).isTrue();
+        });
 
         WishlistPushResponseModel wishlistPushResponse = given()
+                .filter(withCustomAllureFilter())
                 .contentType("application/json")
                 .body(body)
                 .when()
                 .post("https://api.rees46.ru/push")
                 .then()
-//                .log().body();
+                .statusCode(200)
                 .extract().as(WishlistPushResponseModel.class);
 
-        assertThat(wishlistPushResponse.getStatus()).isEqualTo("success");
-        assertThat(wishlistPushResponse.getPayload().getEvent()).isEqualTo("wish");
-        assertThat(wishlistPushResponse.getPayload().getItems().size()).isGreaterThan(1);
+        step("Проверка ответа", () -> {
+            assertThat(wishlistPushResponse.getStatus()).isEqualTo("success");
+            assertThat(wishlistPushResponse.getPayload().getEvent()).isEqualTo("wish");
+            assertThat(wishlistPushResponse.getPayload().getItems().size()).isGreaterThan(0);
+        });
+
+
 
     }
 }
