e7bd3ef4fb4b057dae80e4757eb679f024432483
alex <apribylov90@mail.ru>
2025-04-21 13:47:27 +0300

    added helper method for wishlist remove test

---
+++ src/test/java/ru/alex/bookstore/utils/api/WishlistApi.java
@@ -0,0 +1,36 @@
+package ru.alex.bookstore.utils.api;
+
+import ru.alex.bookstore.api.models.wishlist.WishlistPushResponseModel;
+import ru.alex.bookstore.api.models.wishlist.WishlistResponseModel;
+
+import java.util.Map;
+
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static ru.alex.bookstore.api.filter.CustomAllureFilter.withCustomAllureFilter;
+
+public class WishlistApi {
+
+    public static void addToWishList(Map<String, String> requiredCookies) {
+        String body = "{\"event\":\"wish\",\"shop_id\":\"e33fc52bd51526cc6b0fe4e5e18401\",\"did\":\"AH5aEvmlqn\",\"sid\":\"cIxzHQJbUo\",\"seance\":\"cIxzHQJbUo\",\"segment\":\"B\",\"items\":[{\"id\":\"1133885A\"}],\"referer\":\"https://www.moscowbooks.ru/book/1133885/\"}";
+
+        given()
+                .contentType("application/x-www-form-urlencoded")
+                .cookies(requiredCookies)
+                .formParam("producttype", "0")
+                .formParam("productid", "1133885")
+        .when()
+                .post("https://www.moscowbooks.ru/basket/addwish")
+        .then()
+                .statusCode(200);
+
+        given()
+                .contentType("application/json")
+                .body(body)
+        .when()
+                .post("https://api.rees46.ru/push")
+        .then()
+                .statusCode(200);
+    }
+}
