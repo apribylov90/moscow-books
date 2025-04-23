bcdca44bf8a2cc9616b1ef8cada27d5976502cf8
alex <apribylov90@mail.ru>
2025-04-23 18:37:35 +0300

    tab fix

--- src/test/java/ru/alex/bookstore/utils/api/WishlistApi.java
+++ src/test/java/ru/alex/bookstore/utils/api/WishlistApi.java
@@ -10,21 +10,21 @@ public class WishlistApi {
         String body = "{\"event\":\"wish\",\"shop_id\":\"e33fc52bd51526cc6b0fe4e5e18401\",\"did\":\"AH5aEvmlqn\",\"sid\":\"cIxzHQJbUo\",\"seance\":\"cIxzHQJbUo\",\"segment\":\"B\",\"items\":[{\"id\":\"1133885A\"}],\"referer\":\"https://www.moscowbooks.ru/book/1133885/\"}";
 
         given()
-                .contentType("application/x-www-form-urlencoded")
-                .cookies(requiredCookies)
-                .formParam("producttype", "0")
-                .formParam("productid", "1133885")
+            .contentType("application/x-www-form-urlencoded")
+            .cookies(requiredCookies)
+            .formParam("producttype", "0")
+            .formParam("productid", "1133885")
         .when()
-                .post("https://www.moscowbooks.ru/basket/addwish")
+            .post("https://www.moscowbooks.ru/basket/addwish")
         .then()
                 .statusCode(200);
 
         given()
-                .contentType("application/json")
-                .body(body)
+            .contentType("application/json")
+            .body(body)
         .when()
-                .post("https://api.rees46.ru/push")
+            .post("https://api.rees46.ru/push")
         .then()
-                .statusCode(200);
+            .statusCode(200);
     }
 }
